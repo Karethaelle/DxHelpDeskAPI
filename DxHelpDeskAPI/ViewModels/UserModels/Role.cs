a6d566c184ea7c135462096faa6550c2d7bcdcb8
S.Wilbert <50242813+Karethaelle@users.noreply.github.com>
2023-11-06 10:14:40 +0800

    initial

---
+++ DxHelpDeskAPI/ViewModels/UserModels/Role.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DxHelpDeskAPI.ViewModels.UserModels;
+
+public partial class Role
+{
+    public int Id { get; set; }
+
+    public string? RoleName { get; set; }
+
+    public int? Permissions { get; set; }
+
+    public int? Status { get; set; }
+
+    public string? Description { get; set; }
+}
---
+++ DxHelpDeskAPI/ViewModels/UserModels/Userrole.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DxHelpDeskAPI.ViewModels.UserModels;
+
+public partial class Userrole
+{
+    public int? UserId { get; set; }
+
+    public int? RoleId { get; set; }
+
+    public virtual Role? Role { get; set; }
+
+    public virtual User? User { get; set; }
+}
